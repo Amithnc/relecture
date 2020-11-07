7d443af7d25311fc62e3ad7421fb895ea354d67e
Amithnc <53530365+Amithnc@users.noreply.github.com>
2020-11-07 16:50:56 +0530

    done with the home page

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='/login')
+
 def homepage(request):
     return render (request,'home.html')
 
